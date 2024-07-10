1eb3ef0007cdd11ba32fac36e6dbe8aee47bed62
AqibNiazi <aqibjaved5201@gmail.com>
2024-07-10 19:55:44 +0500

    config files corrected

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@components": path.resolve(__dirname, "src/components"),
-      "@pages": path.resolve(__dirname, "src/pages"),
       "@assets": path.resolve(__dirname, "src/assets"),
       "@layout": path.resolve(__dirname, "src/layout"),
+      "@pages": path.resolve(__dirname, "src/pages"),
       "@store": path.resolve(__dirname, "src/store"),
     },
   },
