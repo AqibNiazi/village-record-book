7135320eb2aea66d854e27a30a8187ed3e04d34c
AqibNiazi <aqibjaved5201@gmail.com>
2024-07-10 17:38:20 +0500

    icons imports added

--- src/pages/DataTable/index.jsx
+++ src/pages/DataTable/index.jsx
@@ -6,25 +6,23 @@ import { FiUsers } from "react-icons/fi";
 import { v4 as uuidv4 } from "uuid";
 import { Link, Outlet, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
-import {
-  Agriculture,
-  Sport,
-  Plane,
-  Warning,
-  BusinessLine,
-  HeartHand,
-  GraduationHat,
-  ActivityHeart,
-  Train,
-  GovtService,
-  Location,
-  GovtTownCity,
-  Coins,
-  WardMarket,
-  CheckList,
-  Villages,
-  Clan,
-} from "@components/icons";
+import Agriculture from "@components/icons/Agriculture";
+import Plane from "@components/icons/Plane";
+import Sport from "@components/icons/Sport";
+import Warning from "@components/icons/Warning";
+import BusinessLine from "@components/icons/BusinessLine";
+import HeartHand from "@components/icons/HeartHand";
+import GraduationHat from "@components/icons/GraduationHat";
+import ActivityHeart from "@components/icons/ActivityHeart";
+import Train from "@components/icons/Train";
+import GovtService from "@components/icons/GovtService";
+import Location from "@components/icons/Location";
+import GovtTownCity from "@components/icons/GovtTownCity";
+import WardMarket from "@components/icons/WardMarket";
+import Coins from "@components/icons/Coins";
+import CheckList from "@components/icons/CheckList";
+import Villages from "@components/icons/Villages";
+import Clan from "@components/icons/Clan";
 
 const Dashboard = () => {
   const location = useLocation();
